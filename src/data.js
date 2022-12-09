c2935ea860c913087a5bf4a13feb1e467b902720
ivyhy98 <ivanahy98@gmail.com>
2022-12-09 15:13:39 -0500

    styling

---
+++ src/data.js
@@ -0,0 +1,36 @@
+const data = [
+  {
+    id: 0,
+    name: "James King",
+    title: "President and CEO",
+    img: "https://cdn5.vectorstock.com/i/1000x1000/51/39/business-woman-standing-cartoon-employee-vector-15325139.jpg",
+    numbers: { work: "555-555-5555", mobile: "111-111-1111" },
+    email: "james.king@work.com",
+  },
+  {
+    id:1,
+    name: "Julie Taylor",
+    title: "VP of Marketing",
+    img: "https://cdn2.vectorstock.com/i/1000x1000/16/61/young-and-successful-business-man-cartoon-employee-vector-15281661.jpg",
+    numbers: { work: "222-222-2222", mobile: "333-333-3333" },
+    email: "julie.taylor@work.com",
+  },
+  {
+    id: 2,
+    name: "Eugene Lee",
+    title: "CFO",
+    img: "https://cdn5.vectorstock.com/i/1000x1000/51/39/business-woman-standing-cartoon-employee-vector-15325139.jpg",
+    numbers: { work: "444-444-4444", mobile: "777-777-7777" },
+    email: "eugene.lee@work.com",
+  },
+  {
+    id: 3,
+    name: "John Williams",
+    title: "VP of Engineering",
+    img: "https://png.pngitem.com/pimgs/s/90-906935_animated-faces-my-hero-design-clip-art-woman.png",
+    numbers: { work: "888-888-8888", mobile: "999-999-9999" },
+    email: "john.williams@work.com",
+  },
+];
+
+export default data;
